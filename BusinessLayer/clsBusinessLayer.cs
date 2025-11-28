5c3519d3d4b49a73e1a6f039c68fbfec17795d54
gksolutions.work007 <gksolutions.work007@gmail.com>
2025-11-28 20:42:38 +0530

    SIGN UP COMPLETE

---
+++ BusinessLayer/clsBusinessLayer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class clsBusinessLayer 
+    {
+        public  string Encrypt(string clearText)
+        {
+            string strReturnValue = string.Empty;
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    strReturnValue = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return strReturnValue;
+        }
+        //Decryption
+        public  string Decrypt(string cipherText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+        public void BL_WriteErrorMsginLog(string strfrmName, string strmsg, string errors)
+        {
+            int Error = System.Runtime.InteropServices.Marshal.GetExceptionCode();
+            BL_LogFileWrite(DateTime.Now + " |" + strfrmName + " | " + strmsg + " | " + errors);
+        }
+        //LOG SYSTEM ERROR MESSAGE
+        public static void BL_LogFileWrite(string message)
+        {
+            FileStream fileStream = null;
+            StreamWriter streamWriter = null;
+            try
+            {
+                string logFilePath = AppDomain.CurrentDomain.BaseDirectory;//  System.Configuration.ConfigurationManager.AppSettings["SupportFilePath"];
+                DirectoryInfo parentDir = Directory.GetParent(logFilePath.EndsWith("\\") ? logFilePath : string.Concat(logFilePath, "\\"));
+                var myParentDir = parentDir.Parent.FullName;
+                string strFol = myParentDir + "\\Log File Errors\\";
+
+                strFol = strFol + "Log System Error" + "-" + DateTime.Today.ToString("ddMMyyyy") + "." + "txt";
+                if (strFol.Equals("")) return;
+                #region Create the Log file directory if it does not exists
+                DirectoryInfo logDirInfo = null;
+                FileInfo logFileInfo = new FileInfo(strFol);
+                logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+                if (!logDirInfo.Exists) logDirInfo.Create();
+                #endregion Create the Log file directory if it does not exists
+                if (!logFileInfo.Exists)
+                {
+                    fileStream = logFileInfo.Create();
+                }
+                else
+                {
+                    fileStream = new FileStream(strFol, FileMode.Append);
+                }
+                streamWriter = new StreamWriter(fileStream);
+                streamWriter.WriteLine(message);
+            }
+            finally
+            {
+                if (streamWriter != null) streamWriter.Close();
+                if (fileStream != null) fileStream.Close();
+            }
+        }
+    }
+}
---
+++ CAVISTAUI/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CAVISTAUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        clsBusinessLayer bl = new clsBusinessLayer();
+        // GET: Login
+        public ActionResult Index(string Msg)
+        {
+            try
+            {
+                string nv = Convert.ToString(Session["NavBarVisible"]);
+                string APIurl = bl.Decrypt(ConfigurationManager.AppSettings["apiurl"].ToString());
+                Session["APIurl"] = APIurl;
+                HttpContext.Session.Add("APIurl", APIurl);
+                string URl = Request.Url.AbsoluteUri;
+                string LASTVAL = URl.Substring(URl.Length - 1);
+                string MU = LASTVAL != "/" ? URl + "/" : URl;
+                Session["AbsURL"] = MU;// Request.Url.AbsoluteUri;
+                if (string.IsNullOrEmpty(nv))
+                {
+                    Session["NavBarVisible"] = "LogOn";
+                }
+                if (!string.IsNullOrEmpty(Msg))
+                {
+                    ViewData["AlertMessage"] = Msg;
+                }                
+            }
+            catch (Exception ex)
+            {
+                bl.BL_WriteErrorMsginLog("Login", "Index", ex.Message);
+            }
+            return View();
+        }
+        public ActionResult AACM(string AAlk)
+        {
+            try
+            {
+                string msg = "";
+                if (!string.IsNullOrEmpty(AAlk))
+                {
+                    string APIurl = bl.Decrypt(ConfigurationManager.AppSettings["apiurl"].ToString());
+                    string uid = bl.Decrypt(AAlk);
+                    HttpClient _client = new HttpClient();
+                    _client.BaseAddress = new Uri(APIurl);// APILink from app config
+                    _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage result = _client.GetAsync("signup/activateaccount?UID=" + uid).Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        // DataTable dtRes = bl.BL_ExecuteParamSP("uspManageUsers", 5, uid);
+                        msg = "You Account is Activated. You can Login now.";
+                        ViewData["AlertMessage"] = msg;
+                    }
+                    else
+                    {
+                        msg = "Account not activated. Try again later.";
+                    }
+                    return RedirectToAction("Index", "Login", new { Msg = msg });
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return View();
+        }
+    }
+}
