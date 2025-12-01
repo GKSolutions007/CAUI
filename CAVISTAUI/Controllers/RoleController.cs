11f29794366be27345885b5b015faf56b7f9875d
GKBSDEV <gkbsdev@gmail.com>
2025-12-01 11:58:40 +0530

    role

---
+++ CAVISTAUI/Controllers/RoleController.cs
@@ -0,0 +1,35 @@
+﻿using CAVISTAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CAVISTAUI.Controllers
+{
+    public class RoleController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            if (Session["LoginUserID"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                DataTable dtPermission = (System.Data.DataTable)Session["dtPermission"];
+                string AddPerm = dtPermission.Select("MenuID = 7", null).Length > 0 ? "1" : "0";
+                string ModPerm = dtPermission.Select("MenuID = 8", null).Length > 0 ? "1" : "0";
+                string ViewPerm = dtPermission.Select("MenuID = 9", null).Length > 0 ? "1" : "0";           
+                SingleMasterModel dam = new SingleMasterModel();
+                dam.FormName = "Users";
+                dam.Add = AddPerm;
+                dam.Modify = ModPerm;
+                dam.View = ViewPerm;
+                return View(dam);
+            }
+        }
+    }
+}
